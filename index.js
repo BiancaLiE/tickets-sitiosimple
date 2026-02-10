53b8acaabc26fbbda68398e791f33f03dcf44c8f
BiancaLiE <biancaliescalera@gmail.com>
2026-02-10 16:33:27 -0300

    index.js

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ app.post("/login", (req, res) => {
   res.status(401).json({ ok: false, error: "Credenciales inválidas" });
 });
 
+app.post("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.json({ ok: true });
+  });
+});
+
 app.get("/me", (req, res) => {
   if (req.session.user) {
     res.json({
@@ -191,6 +197,7 @@ app.put("/tickets/:pedidoId", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en puerto ${PORT}`);
 });
+
 
 
 
