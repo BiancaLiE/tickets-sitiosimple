4c6756cae3f315685ab91ed36c1f4476bd6e88a9
BiancaLiE <biancaliescalera@gmail.com>
2026-03-10 01:54:27 -0300

    index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ async function connectDB() {
   await client.connect();
   const db = client.db("ticketsDB");
   ticketsCollection = db.collection("tickets");
-  await ticketsCollection.createIndex({ pedidoId: 1 }, { unique: true });
   console.log("✅ Conectado a MongoDB");
 }
 
@@ -157,15 +156,8 @@ app.post("/webhook", async (req, res) => {
       creadoEn: new Date()
     };
 
-    try {
-      await ticketsCollection.insertOne(ticket);
-    } catch (err) {
-      if (err.code === 11000) {
-        console.log("⚠️ Pedido duplicado ignorado:", ticket.pedidoId);
-        return res.sendStatus(200);
-      }
-      throw err;
-    }
+    
+    await ticketsCollection.insertOne(ticket);
 
     // -----------------------------
     // Limitar colección a 5000 tickets
@@ -286,6 +278,7 @@ connectDB()
     console.error("❌ Error conectando a MongoDB:", err);
     process.exit(1);
   });
+
 
 
 
